aa0db9d599e72f02e4a9d8ffd85292be9db4ee09
allnestir80-png <allnestir80@gmail.com>
2026-02-24 13:40:47 +0500

    Update5 sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'telegram-inspection-v1.1';
+const CACHE_NAME = 'telegram-inspection-v2.0';
 const ASSETS = [
     './',
     './index.html',
@@ -27,5 +27,4 @@ self.addEventListener('fetch', (e) => {
     e.respondWith(
         caches.match(e.request).then((response) => response || fetch(e.request))
     );
-
 });
